f60e644ecb9cf00b94b2cc22e8afd5c471cbebed
Sima Altınkum <simaaltinkum02@gmail.com>
2024-08-05 15:44:22 +0300

    update

---
+++ myaccount/companies/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("add-companies/", views.add_company, name="index"),
+    path("select/", views.selectCompany, name="select"),
+    path(
+        "select/detail/<int:pk>/", views.selectCompanyDetail, name="selectCompanyDetail"
+    ),
+    path("operation/<int:pk>/", views.money, name="money"),
+    path("add-operation/", views.addOperation, name="addop"),
+    path("show-money/", views.showMoney, name="showMoney"),
+    path("show-info/", views.showInfo, name="showInfo"),
+]
